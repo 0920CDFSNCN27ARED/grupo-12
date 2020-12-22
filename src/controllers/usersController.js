204ac0809bdb5a66153c543f12b4c904b7bd9d9e
santi-nihany <santi.nihany@gmail.com>
2020-12-22 17:01:43 -0300

    Validation on register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,9 @@
 const getUsers = require("../utils/getUsers");
 const fs = require("fs");
 const path = require("path");
+const { validationResult } = require("express-validator");
 const usersFilePath = path.join(__dirname, "../data/usersDB.json");
+const bcrypt = require("bcrypt");
 
 const usersController = {
     getLogin: function(req, res, next) {
@@ -11,20 +13,26 @@ const usersController = {
         res.render('users/register');
     },
     register:function(req, res, next) {
-        let users= getUsers();
-        const newId = users.length !=0 ? users[users.length - 1].id + 1 : 1;
-        let user = {
-            id : newId,
-            name: req.body.name,
-            userName: req.body.userName,
-            phone: req.body.selectNumber + req.body.phoneNumber,
-            email: req.body.email,
-            password: req.body.password,
-        };
-        users.push(user);
-        let usersJSON = JSON.stringify(users);
-        fs.writeFileSync(usersFilePath, usersJSON);
-        res.redirect("login");
+        let errors = validationResult(req);
+        if(errors.isEmpty()){
+            let users= getUsers();
+            const newId = users.length !=0 ? users[users.length - 1].id + 1 : 1;
+            let user = {
+                id : newId,
+                name: req.body.name,
+                userName: req.body.userName,
+                phone: req.body.selectNumber + req.body.phoneNumber,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password,10),
+                avatar: req.files ? req.files[0].filename: "default-avatar.png",
+            };
+            users.push(user);
+            let usersJSON = JSON.stringify(users);
+            fs.writeFileSync(usersFilePath, usersJSON);
+            res.redirect("login");
+        }else{
+            res.render("users/register",{errors:errors.errors})
+        }
     },
     confirmation:function(req, res, next) {
         res.render('users/confirmation');
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const usersController = require('../controllers/usersController');
 const router = express.Router();
+const {check,validationResult, body}= require("express-validator");
+const multer = require('multer');
+const confirmPassword = require('../middlewares/confirmPassword')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images/products')
+    },
+    filename: function (req, file, cb) {
+      cb(null,file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    }
+  });
+   
+  var upload = multer({ storage: storage });
 
 /* GET login page. */
 router.get('/login', usersController.getLogin);
 
 /* GET register page. */
 router.get('/register', usersController.getRegister);
-router.post('/register', usersController.register)
+router.post('/register',upload.any(),[
+    check("name").isLength({min:4,max:50}).withMessage("El nombre debe tener entre 4 y 50 caracteres de largo"),
+    check("userName").isLength({min:4,max:15}).withMessage("El nombre de usuario debe tener entre 4 y 15 caracteres de largo"),
+    check("email").isEmail().withMessage("Email inválido"),
+    check("phoneNumber").isMobilePhone().withMessage("Numero de telefono inválido"),
+    check("password").isLength({min:8}).isAlphanumeric().withMessage("Contraseña inválida: minimo 8 caracteres,letras(a-zA-Z) y números"), 
+],confirmPassword,usersController.register);
 
 /* GET profile page. */
 router.get('/profile', usersController.profile);
